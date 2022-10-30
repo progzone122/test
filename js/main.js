5b846eadcd7d4bccfa40e9a2b3f412fdc4d47c4d
Diablo <80642969+progzone122@users.noreply.github.com>
2022-10-30 13:39:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const url_getParams = new URLSearchParams(url.search.substring(1));
 let iframe_params = "";
 
 url_getParams.forEach((value, key) => {
-    iframe_params += `${ key }=${ value } `;
+    iframe_params += `${ key }=` + `"` + `${ value } ` + `"`;
 });
 console.log(iframe_params);
 $("#player").append(`<div id="yohoho" style="width:1350px;height:370px;border:0;margin:0;padding:0;overflow:hidden;position:relative" ${ iframe_params }></div>`);
